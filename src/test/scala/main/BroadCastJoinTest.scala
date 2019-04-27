635553f786208767cf343edd61c74fc20adf9240
Sanjyot <in-sanjyot.bandal@Sanjyots-MacBook-Pro.local>
2019-04-27 22:28:38 +0530

    Implemented code to pass failing tests

--- src/test/scala/main/BroadCastJoinTest.scala
+++ src/test/scala/main/BroadCastJoinTest.scala
@@ -1,19 +1,28 @@
 package main
 
+import java.lang
+
+import org.apache.spark.sql.Dataset
 import org.apache.spark.sql.catalyst.plans.Inner
 import org.apache.spark.sql.execution.joins.{BroadcastHashJoinExec, BroadcastNestedLoopJoinExec, SortMergeJoinExec}
 import org.apache.spark.sql.internal.SQLConf
 
 class BroadCastJoinTest extends SparkTest {
+
   import testImplicits._
 
   test("Joining Two smaller Data Sets") {
     val thresholdSettings = spark.conf.get(SQLConf.AUTO_BROADCASTJOIN_THRESHOLD.key).toLong
+    //by default 10 MB for AUTO_BROADCASTJOIN_THRESHOLD
+    //If size less than 10 MB of either DF then that will be broad casted
+    //spark.conf.set(SQLConf.AUTO_BROADCASTJOIN_THRESHOLD.key,-1)
     SizeInBytes(thresholdSettings).toMBs.prettyPrint()
-    val df2 = spark.range(100).as("b")
+    val df2: Dataset[lang.Long] = spark.range(100).as("b")
     val df1 = spark.range(100).as("a")
     val joinedDF = df1.join(df2).where($"a.id" === $"b.id")
-    assert(joinedDF.queryExecution.sparkPlan.collect { case p: BroadcastHashJoinExec => p }.size === 0)
+    assert(joinedDF.queryExecution.sparkPlan.collect { case p: BroadcastHashJoinExec => p }.size === 1)
+//    println(joinedDF.queryExecution.sparkPlan)
+//    println(joinedDF.explain())
   }
 
   test("Joining one large with another large Data Set") {
@@ -27,8 +36,11 @@ class BroadCastJoinTest extends SparkTest {
     val peopleDF2 = spark.read.option("header", "true")
       .csv(Resource.pathOf("people.csv"))
 
-      val joinedDF = peopleDF.join(peopleDF2, Seq("firstName"), Inner)
-      assert(joinedDF.queryExecution.sparkPlan.collect { case p: BroadcastHashJoinExec => p }.size === 1)
+    //Joining 2 large data sets should not ideally result in Broadcast, shuffle is ok in this case
+    val joinedDF = peopleDF.join(peopleDF2, Seq("firstName"), Inner)
+    assert(joinedDF.queryExecution.sparkPlan.collect { case p: BroadcastHashJoinExec => p }.size === 0)
+//    println(joinedDF.explain())
+//    println(joinedDF.queryExecution.sparkPlan)
   }
 
   test("Joining one large with one medium Data Sets") {
@@ -44,22 +56,29 @@ class BroadCastJoinTest extends SparkTest {
 
     println(peopleDF.queryExecution.logical.stats.sizeInBytes)
     println(peopleSmallDF.queryExecution.logical.stats.sizeInBytes)
-    withSQLConf(SQLConf.AUTO_BROADCASTJOIN_THRESHOLD.key -> SizeInMBs(3L).toBytes.toString) {
+    //Size of both the csv > 3mb so no broadcast, but if we increase size then possible
+    withSQLConf(SQLConf.AUTO_BROADCASTJOIN_THRESHOLD.key -> SizeInMBs(4L).toBytes.toString) {
       val joinedDF = peopleDF.join(peopleSmallDF, Seq("firstName"), Inner)
       assert(joinedDF.queryExecution.sparkPlan.collect { case p: BroadcastHashJoinExec => p }.size === 1)
     }
   }
+  import org.apache.spark.sql.functions._
+
 
   test("Joining Two smaller Data Sets with broadcast threshold off") {
     val thresholdSettings = spark.conf.get(SQLConf.AUTO_BROADCASTJOIN_THRESHOLD.key).toLong
     SizeInBytes(thresholdSettings).toMBs.prettyPrint()
-    withSQLConf(SQLConf.AUTO_BROADCASTJOIN_THRESHOLD.key -> "-1") {
+    withSQLConf(SQLConf.AUTO_BROADCASTJOIN_THRESHOLD.key -> "-1"/*SizeInMBs(1L).toBytes.toString*/) {
       val df2 = spark.range(100).as("b")
       val df1 = spark.range(100).as("a")
-      val joinedDF = df1.join(df2).where($"a.id" === $"b.id")
+      //broadcast() function can override settings used above
+      val joinedDF = df1.join(broadcast(df2)).where($"a.id" === $"b.id")
+//      println(joinedDF.explain())
+//      println(joinedDF.queryExecution.sparkPlan)
       assert(joinedDF.queryExecution.sparkPlan.collect { case p: BroadcastHashJoinExec => p }.size === 1)
       // Please don't change below assert to fix test
       assert(joinedDF.queryExecution.sparkPlan.collect { case p: SortMergeJoinExec => p }.size === 0)
+
     }
   }
 
@@ -71,7 +90,7 @@ class BroadCastJoinTest extends SparkTest {
       .csv(Resource.pathOf("people_small.csv"))
 
     withSQLConf(SQLConf.AUTO_BROADCASTJOIN_THRESHOLD.key -> SizeInMBs(3L).toBytes.toString) {
-      val joinedDF = peopleDF.crossJoin(peopleSmallDF)
+      val joinedDF = peopleDF.crossJoin(broadcast(peopleSmallDF))
       assert(joinedDF.queryExecution.sparkPlan.collect { case p: BroadcastNestedLoopJoinExec => p }.size === 1)
     }
   }
--- src/test/scala/main/JoinTest.scala
+++ src/test/scala/main/JoinTest.scala
@@ -1,5 +1,8 @@
 package main
 
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.catalyst.plans.Cross
+
 class JoinTest extends SparkTest {
 
   import testImplicits._
@@ -22,16 +25,31 @@ class JoinTest extends SparkTest {
       (6, 7, 50.0)
     ).toDF("orderId", "customerId", "amount")
 
-    def genders = Seq(
+    def genders: DataFrame = Seq(
       ("M", "Male"),
       ("F", "Female"),
       ("O", "Other")
     ).toDF("genderCode", "gender")
   }
 
+  import TestData._
+
   test("orders with customer names and genders") {
 
-    val actualDF = spark.emptyDataFrame // Replace this with actual code to pass tests
+    val actualDF = customers.join(orders, "customerId").join(genders, "genderCode")
+    //alternative using select query
+//    val actualDF = orders
+//      .join(customers, "customerId")
+//      .join(genders, "genderCode")
+//      .select("genderCode", "customerId", "name", "orderId", "amount", "gender")
+
+    //TODO :: Sequence of columns in join
+    //First the join attribute, then remaining columns of left table in order (left to right) followed by right table
+    //println(customers.join(orders,"customerId").show(1))
+    //println(orders.join(customers,"customerId").show(1))
+
+
+    //println(actualDF.show())
 
     val expectedDF = Seq(
       ("M", 1, "Alex", 1, 5.0, "Male"),
@@ -46,8 +64,7 @@ class JoinTest extends SparkTest {
 
   test("all unique customers with minimum one order") {
 
-    val actualDF = spark.emptyDataFrame // Replace this with actual code to pass tests
-
+    val actualDF = customers.join(orders, "customerId").select("customerId", "name", "genderCode").distinct()
     val expectedDF = Seq(
       (1, "Alex", "M"),
       (2, "Bill", "M")
@@ -58,23 +75,25 @@ class JoinTest extends SparkTest {
 
   test("all possible customer and orders combinations") {
 
-    val actualDF = spark.emptyDataFrame // Replace this with actual code to pass tests
-
-    val expectedDF = Seq((1,"Alex","Female"),
-      (1,"Alex","Male"),
-      (1,"Alex","Other"),
-      (2,"Bill","Female"),
-      (2,"Bill","Male"),
-      (2,"Bill","Other"),
-      (3,"Steffi","Female" ),
-      (3,"Steffi","Male"),
-      (3,"Steffi","Other"),
-      (4,"Mary","Female"),
-      (4,"Mary","Male"),
-      (4,"Mary","Other"),
-      (5,"Julia","Female"),
-      (5,"Julia","Male"),
-      (5,"Julia","Other")).toDF("customerId", "name", "gender")
+    val actualDF = customers.crossJoin(genders).select("customerId", "name", "gender")
+    //filter on columns of interest and then join
+    val actualDF1 = customers.select("customerId", "name").crossJoin(genders.select("gender")) //spark.emptyDataFrame // Replace this with actual code to pass tests
+    //val actualDF2 = customers.join(genders, Seq(), Cross)
+    val expectedDF = Seq((1, "Alex", "Female"),
+      (1, "Alex", "Male"),
+      (1, "Alex", "Other"),
+      (2, "Bill", "Female"),
+      (2, "Bill", "Male"),
+      (2, "Bill", "Other"),
+      (3, "Steffi", "Female"),
+      (3, "Steffi", "Male"),
+      (3, "Steffi", "Other"),
+      (4, "Mary", "Female"),
+      (4, "Mary", "Male"),
+      (4, "Mary", "Other"),
+      (5, "Julia", "Female"),
+      (5, "Julia", "Male"),
+      (5, "Julia", "Other")).toDF("customerId", "name", "gender")
 
     checkAnswer(actualDF, expectedDF)
   }
